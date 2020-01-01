b07c9c5e68dc823b7780236c9056757d3512443a
MURAOKA Taro <koron.kaoriya@gmail.com>
2020-01-01 22:50:18 +0900

    impl. IN/OUT group and most of control group

--- ctrl.go
+++ ctrl.go
@@ -1,5 +1,154 @@
 package z80
 
 var ctrl = []*OPCode{
-	// TODO: implement me.
+
+	{
+		N: "DAA",
+		C: []Code{
+			{0x27, 0x00, nil},
+		},
+		T: []int{4},
+		F: func(cpu *CPU, codes []uint8) {
+			// TODO: implmenet "DAA"
+		},
+	},
+
+	{
+		N: "CPL",
+		C: []Code{
+			{0x2F, 0x00, nil},
+		},
+		T: []int{4},
+		F: func(cpu *CPU, codes []uint8) {
+			cpu.AF.Hi = ^cpu.AF.Hi
+			cpu.flagUpdate(FlagOp{}.Set(H).Set(N))
+		},
+	},
+
+	{
+		N: "NEG",
+		C: []Code{
+			{0xed, 0x00, nil},
+			{0x44, 0x00, nil},
+		},
+		T: []int{4, 4},
+		F: func(cpu *CPU, codes []uint8) {
+			a := cpu.AF.Hi
+			v := ^a + 1
+			cpu.AF.Hi = v
+			cpu.flagUpdate(FlagOp{}.
+				Put(S, v&0x80 != 0).
+				Put(Z, v == 0).
+				// FIXME: check and fix H flag behavior.
+				Put(H, false).
+				Put(PV, a == 0x80).
+				Set(N).
+				Put(C, a != 0))
+		},
+	},
+
+	{
+		N: "CCF",
+		C: []Code{
+			{0x3f, 0x00, nil},
+		},
+		T: []int{4},
+		F: func(cpu *CPU, codes []uint8) {
+			c := cpu.flag(C)
+			cpu.flagUpdate(FlagOp{}.Put(H, c).Reset(N).Put(C, !c))
+		},
+	},
+
+	{
+		N: "SCF",
+		C: []Code{
+			{0x37, 0x00, nil},
+		},
+		T: []int{4},
+		F: func(cpu *CPU, codes []uint8) {
+			cpu.flagUpdate(FlagOp{}.Reset(H).Reset(N).Set(C))
+		},
+	},
+
+	{
+		N: "NOP",
+		C: []Code{
+			{0x00, 0x00, nil},
+		},
+		T: []int{4},
+		F: func(cpu *CPU, codes []uint8) {
+			// do nothing.
+		},
+	},
+
+	{
+		N: "HALT",
+		C: []Code{
+			{0x76, 0x00, nil},
+		},
+		T: []int{4},
+		F: func(cpu *CPU, codes []uint8) {
+			cpu.PC--
+		},
+	},
+
+	{
+		N: "DI",
+		C: []Code{
+			{0xf3, 0x00, nil},
+		},
+		T: []int{4},
+		F: func(cpu *CPU, codes []uint8) {
+			cpu.IFF1 = false
+			cpu.IFF2 = false
+		},
+	},
+
+	{
+		N: "EI",
+		C: []Code{
+			{0xfb, 0x00, nil},
+		},
+		T: []int{4},
+		F: func(cpu *CPU, codes []uint8) {
+			cpu.IFF1 = true
+			cpu.IFF2 = true
+		},
+	},
+
+	{
+		N: "IM 0",
+		C: []Code{
+			{0xed, 0x00, nil},
+			{0x46, 0x00, nil},
+		},
+		T: []int{4, 4},
+		F: func(cpu *CPU, codes []uint8) {
+			// TODO: implement "IM 0"
+		},
+	},
+
+	{
+		N: "IM 1",
+		C: []Code{
+			{0xed, 0x00, nil},
+			{0x56, 0x00, nil},
+		},
+		T: []int{4, 4},
+		F: func(cpu *CPU, codes []uint8) {
+			// TODO: implement "IM 1"
+		},
+	},
+
+	{
+		N: "IM 2",
+		C: []Code{
+			{0xed, 0x00, nil},
+			{0x5e, 0x00, nil},
+		},
+		T: []int{4, 4},
+		F: func(cpu *CPU, codes []uint8) {
+			// TODO: implement "IM 2"
+		},
+	},
 }
--- inout.go
+++ inout.go
@@ -1,5 +1,216 @@
 package z80
 
+import "math/bits"
+
 var inout = []*OPCode{
-	// TODO: implement me.
+
+	{
+		N: "IN A, (n)",
+		C: []Code{
+			{0xdb, 0x00, nil},
+			{0x00, 0xff, nil},
+		},
+		T: []int{4, 3, 4},
+		F: func(cpu *CPU, codes []uint8) {
+			cpu.AF.Hi = cpu.IO.In(codes[1])
+		},
+	},
+
+	{
+		N: "IN r (C)",
+		C: []Code{
+			{0xed, 0x00, nil},
+			{0x40, 0x38, vReg8_3},
+		},
+		T: []int{4, 4, 4},
+		F: func(cpu *CPU, codes []uint8) {
+			v := cpu.IO.In(cpu.BC.Lo)
+			// FIXME: support 0x06 to apply flags only.
+			r := cpu.regP(codes[1] >> 3)
+			*r = v
+			cpu.flagUpdate(FlagOp{}.
+				Put(S, v&0x80 != 0).
+				Put(Z, v == 0).
+				Reset(H).
+				Put(PV, bits.OnesCount8(v)%2 == 0).
+				Reset(N))
+		},
+	},
+
+	{
+		N: "INI",
+		C: []Code{
+			{0xed, 0x00, nil},
+			{0xa2, 0x00, nil},
+		},
+		T: []int{4, 5, 3, 4},
+		F: func(cpu *CPU, codes []uint8) {
+			cpu.Memory.Set(cpu.HL.U16(), cpu.IO.In(cpu.BC.Hi))
+			cpu.BC.Hi--
+			cpu.HL.SetU16(cpu.HL.U16() + 1)
+			cpu.flagUpdate(FlagOp{}.
+				Put(Z, cpu.BC.Hi == 0).
+				Set(N))
+		},
+	},
+
+	{
+		N: "INIR",
+		C: []Code{
+			{0xed, 0x00, nil},
+			{0xb2, 0x00, nil},
+		},
+		T:  []int{4, 5, 3, 4, 5},
+		T2: []int{4, 5, 3, 4},
+		F: func(cpu *CPU, codes []uint8) {
+			cpu.Memory.Set(cpu.HL.U16(), cpu.IO.In(cpu.BC.Hi))
+			cpu.BC.Hi--
+			cpu.HL.SetU16(cpu.HL.U16() + 1)
+			cpu.flagUpdate(FlagOp{}.
+				Put(Z, cpu.BC.Hi == 0).
+				Set(N))
+			if cpu.BC.Hi != 0 {
+				cpu.PC -= 2
+			}
+		},
+	},
+
+	{
+		N: "IND",
+		C: []Code{
+			{0xed, 0x00, nil},
+			{0xaa, 0x00, nil},
+		},
+		T: []int{4, 5, 3, 4},
+		F: func(cpu *CPU, codes []uint8) {
+			cpu.Memory.Set(cpu.HL.U16(), cpu.IO.In(cpu.BC.Hi))
+			cpu.BC.Hi--
+			cpu.HL.SetU16(cpu.HL.U16() - 1)
+			cpu.flagUpdate(FlagOp{}.
+				Put(Z, cpu.BC.Hi == 0).
+				Set(N))
+		},
+	},
+
+	{
+		N: "INDR",
+		C: []Code{
+			{0xed, 0x00, nil},
+			{0xba, 0x00, nil},
+		},
+		T:  []int{4, 5, 3, 4, 5},
+		T2: []int{4, 5, 3, 4},
+		F: func(cpu *CPU, codes []uint8) {
+			cpu.Memory.Set(cpu.HL.U16(), cpu.IO.In(cpu.BC.Hi))
+			cpu.BC.Hi--
+			cpu.HL.SetU16(cpu.HL.U16() - 1)
+			cpu.flagUpdate(FlagOp{}.
+				Put(Z, cpu.BC.Hi == 0).
+				Set(N))
+			if cpu.BC.Hi != 0 {
+				cpu.PC -= 2
+			}
+		},
+	},
+
+	{
+		N: "OUT (n), A",
+		C: []Code{
+			{0xd3, 0x00, nil},
+			{0x00, 0xff, nil},
+		},
+		T: []int{4, 3, 4},
+		F: func(cpu *CPU, codes []uint8) {
+			cpu.IO.Out(codes[1], cpu.AF.Hi)
+		},
+	},
+
+	{
+		N: "OUT (C), r",
+		C: []Code{
+			{0xed, 0x00, nil},
+			{0x41, 0x38, vReg8_3},
+		},
+		T: []int{4, 4, 4},
+		F: func(cpu *CPU, codes []uint8) {
+			r := cpu.regP(codes[1] >> 3)
+			cpu.IO.Out(cpu.BC.Lo, *r)
+		},
+	},
+
+	{
+		N: "OUTI",
+		C: []Code{
+			{0xed, 0x00, nil},
+			{0xa3, 0x00, nil},
+		},
+		T: []int{4, 5, 3, 4},
+		F: func(cpu *CPU, codes []uint8) {
+			cpu.IO.Out(cpu.BC.Lo, cpu.Memory.Get(cpu.HL.U16()))
+			cpu.BC.Hi--
+			cpu.HL.SetU16(cpu.HL.U16() + 1)
+			cpu.flagUpdate(FlagOp{}.
+				Put(Z, cpu.BC.Hi == 0).
+				Set(N))
+		},
+	},
+
+	{
+		N: "OTIR",
+		C: []Code{
+			{0xed, 0x00, nil},
+			{0xb3, 0x00, nil},
+		},
+		T:  []int{4, 5, 3, 4, 5},
+		T2: []int{4, 5, 3, 4},
+		F: func(cpu *CPU, codes []uint8) {
+			cpu.IO.Out(cpu.BC.Lo, cpu.Memory.Get(cpu.HL.U16()))
+			cpu.BC.Hi--
+			cpu.HL.SetU16(cpu.HL.U16() + 1)
+			cpu.flagUpdate(FlagOp{}.
+				Put(Z, cpu.BC.Hi == 0).
+				Set(N))
+			if cpu.BC.Hi != 0 {
+				cpu.PC -= 2
+			}
+		},
+	},
+
+	{
+		N: "OUTD",
+		C: []Code{
+			{0xed, 0x00, nil},
+			{0xab, 0x00, nil},
+		},
+		T: []int{4, 5, 3, 4},
+		F: func(cpu *CPU, codes []uint8) {
+			cpu.IO.Out(cpu.BC.Lo, cpu.Memory.Get(cpu.HL.U16()))
+			cpu.BC.Hi--
+			cpu.HL.SetU16(cpu.HL.U16() - 1)
+			cpu.flagUpdate(FlagOp{}.
+				Put(Z, cpu.BC.Hi == 0).
+				Set(N))
+		},
+	},
+
+	{
+		N: "OTDR",
+		C: []Code{
+			{0xed, 0x00, nil},
+			{0xbb, 0x00, nil},
+		},
+		T:  []int{4, 5, 3, 4, 5},
+		T2: []int{4, 5, 3, 4},
+		F: func(cpu *CPU, codes []uint8) {
+			cpu.IO.Out(cpu.BC.Lo, cpu.Memory.Get(cpu.HL.U16()))
+			cpu.BC.Hi--
+			cpu.HL.SetU16(cpu.HL.U16() - 1)
+			cpu.flagUpdate(FlagOp{}.
+				Put(Z, cpu.BC.Hi == 0).
+				Set(N))
+			if cpu.BC.Hi != 0 {
+				cpu.PC -= 2
+			}
+		},
+	},
 }
