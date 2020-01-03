ed9412bb268892508f1bfa574bec5f7a25b4adab
MURAOKA Taro <koron.kaoriya@gmail.com>
2020-01-03 16:44:21 +0900

    comments

--- dumb.go
+++ dumb.go
@@ -1,7 +1,9 @@
 package z80
 
+// DumbMemory provides Memory interface as wrapper of []uint8
 type DumbMemory []uint8
 
+// Get gets a byte at addr of memory.
 func (dm DumbMemory) Get(addr uint16) uint8 {
 	if int(addr) >= len(dm) {
 		return 0
@@ -9,6 +11,7 @@ func (dm DumbMemory) Get(addr uint16) uint8 {
 	return dm[addr]
 }
 
+// Set sets a byte at addr of memory.
 func (dm DumbMemory) Set(addr uint16, value uint8) {
 	if int(addr) >= len(dm) {
 		return
@@ -18,8 +21,10 @@ func (dm DumbMemory) Set(addr uint16, value uint8) {
 
 var _ Memory = DumbMemory(nil)
 
+// DumbIO provides IO interface as wrapper of []uint8
 type DumbIO []uint8
 
+// In gets a byte at addr of IO
 func (dio DumbIO) In(addr uint8) uint8 {
 	if int(addr) >= len(dio) {
 		return 0
@@ -27,6 +32,7 @@ func (dio DumbIO) In(addr uint8) uint8 {
 	return dio[addr]
 }
 
+// Out sets a byte at addr of IO
 func (dio DumbIO) Out(addr uint8, value uint8) {
 	if int(addr) >= len(dio) {
 		return
